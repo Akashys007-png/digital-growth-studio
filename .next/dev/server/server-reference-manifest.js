70b1a81f8d760c3ca2b08377144f6666475cd8a7
Akash <Akashys@csu.fullerton.edu>
2026-07-24 21:53:18 -0700

    Remove .next from repository

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Q/Jo209eAnGgb9WVpugrQCFevahKKfSuKQgxqgAX2bo=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_10mos2a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_1zxnh6y._.js","static/chunks/node_modules_lucide-react_dist_esm_icons_162-9uj._.js"],
-    source: "dynamic"
-});
